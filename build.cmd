8e31d146655a5124bd2397cb1525e7b936d4aad7
antiufo <antiufo@localhost>
2011-11-01 12:53:26 +0100

    build script

---
+++ build.cmd
@@ -0,0 +1,20 @@
+
+
+@echo off
+cd /d %~dp0
+
+"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" TrackFolderChanges\TrackFolderChanges.sln  /p:Configuration=Release
+
+
+
+set zip=%cd%\TrackFolderChanges.zip
+
+del "%zip%"
+
+cd TrackFolderChanges\bin\Release
+
+%Apps%\7-Zip\7z.exe a "%zip%" *.dll *.exe *.config -aoa
+
+
+
+
